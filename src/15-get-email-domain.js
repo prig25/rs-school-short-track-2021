a76aaf0a8da7cb356b9344b2c6101a4fd1d813d2
prig25 <5446068@gmail.com>
2021-04-12 19:52:15 +0300

    feat: implement function

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,12 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  // return email.slice(email.indexOf('@') + 1, email.length);
+  for (let i = email.length - 1; i >= 0; i--) {
+    if (email[i] === '@') return email.slice(i + 1, email.length);
+  }
+  return 'not found domain';
 }
 
 module.exports = getEmailDomain;
