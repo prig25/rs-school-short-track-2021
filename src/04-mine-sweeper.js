a81037e4abb1ec1caaa5160b3ecdf602b1294b87
prig25 <5446068@gmail.com>
2021-04-12 23:58:08 +0300

    feat: implement function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,154 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const arr = [];
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    arr.push(new Array(matrix[i].length).fill(0)); // создаем матрицу
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0 && j === 0) {
+        if (matrix[i][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j] === true) {
+          count++;
+        }
+      } else if (i === 0 && j > 0 && j < matrix[i].length - 1) {
+        if (matrix[i][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i][j + 1] === true) {
+          count++;
+        }
+      } else if (i === 0 && j === matrix[i].length - 1) {
+        if (matrix[i][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j] === true) {
+          count++;
+        }
+      } else if (i > 0 && i < matrix.length - 1 && j === matrix[i].length - 1) {
+        if (matrix[i - 1][j] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j] === true) {
+          count++;
+        }
+      } else if (i === matrix.length - 1 && j === matrix[i].length - 1) {
+        if (matrix[i - 1][j] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i][j - 1] === true) {
+          count++;
+        }
+      } else if (i === matrix.length - 1 && j > 0 && j < matrix[i].length - 1) {
+        if (matrix[i][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i][j - 1] === true) {
+          count++;
+        }
+      } else if (i === matrix.length - 1 && j === 0) {
+        if (matrix[i][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j] === true) {
+          count++;
+        }
+      } else if (i > 0 && i < matrix.length - 1 && j === 0) {
+        if (matrix[i + 1][j] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j] === true) {
+          count++;
+        }
+      } else {
+        if (matrix[i][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j + 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j] === true) {
+          count++;
+        }
+        if (matrix[i + 1][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j - 1] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j] === true) {
+          count++;
+        }
+        if (matrix[i - 1][j + 1] === true) {
+          count++;
+        }
+      }
+      arr[i][j] = count;
+      count = 0;
+    }
+  }
+  return arr;
 }
 
 module.exports = minesweeper;
+
+// const matrix = [
+//   [true, false, false],
+//   [false, true, false],
+//   [false, false, false],
+// ];
+// minesweeper(matrix);
